700c2885d06e31199dfa90bde4f46f2bcef04556
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2023-01-05 16:21:45 +0100

    Update kubernetes_provider.tf

--- kubernetes_provider.tf
+++ kubernetes_provider.tf
@@ -1,26 +1,18 @@
 data "aws_eks_cluster" "this" {
-  name = aws_eks_cluster.this[0].cluster_id
+  name = aws_eks_cluster.this[0].name
   depends_on = [
     module.eks_managed_node_group
   ]
 }
 
-# data "aws_eks_cluster_auth" "this" {
-#   name = aws_eks_cluster.this[0].cluster_id
-#   depends_on = [
-#     module.eks_managed_node_group
-#   ]
-# }
-
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.0.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.0.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.0.name]
+    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.name]
   }
 }
