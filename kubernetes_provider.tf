e965d59aed715a61e5ecbe83eb4c48eeb205a3e9
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2023-01-10 13:22:00 +0100

    Update kubernetes_provider.tf

--- kubernetes_provider.tf
+++ kubernetes_provider.tf
@@ -10,10 +10,13 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.this[0].endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this[0].certificate_authority.0.data)
 
-  exec {
+  dynamic "exec" {
+    for_each = var.create ? [1] : []
+    content {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
     args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this[0].name]
+    }
   }
 }
