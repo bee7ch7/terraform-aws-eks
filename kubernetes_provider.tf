2b0fb3ecf20dd4109960bd80f04e912aa2369b3c
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2023-02-20 21:38:51 +0100

    Update kubernetes_provider.tf

--- kubernetes_provider.tf
+++ kubernetes_provider.tf
@@ -1,23 +1,23 @@
-data "aws_eks_cluster" "this" {
-  count = var.create ? 1 : 0
-  name = aws_eks_cluster.this[0].name
-  depends_on = [
-    module.eks_managed_node_group,
-    aws_eks_cluster.this
-  ]
-}
+# data "aws_eks_cluster" "this" {
+#   count = var.create ? 1 : 0
+#   name = aws_eks_cluster.this[0].name
+#   depends_on = [
+#     module.eks_managed_node_group,
+#     aws_eks_cluster.this
+#   ]
+# }
 
-provider "kubernetes" {
-  host                   = try(data.aws_eks_cluster.this[0].endpoint, "")
-  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.this[0].certificate_authority.0.data), "")
+# provider "kubernetes" {
+#   host                   = try(data.aws_eks_cluster.this[0].endpoint, "")
+#   cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.this[0].certificate_authority.0.data), "")
 
-  dynamic "exec" {
-    for_each = var.create ? [1] : []
-    content {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this[0].name]
-    }
-  }
-}
+#   dynamic "exec" {
+#     for_each = var.create ? [1] : []
+#     content {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     command     = "aws"
+#     # This requires the awscli to be installed locally where Terraform is executed
+#     args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this[0].name]
+#     }
+#   }
+# }
